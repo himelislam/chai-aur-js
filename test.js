2e137f3b2034a9ce05b255683d00e4d9fa76fbe2
Himel Islam <himel7100@gmail.com>
2023-12-11 00:31:40 +0600

    for loop

---
+++ test.js
@@ -0,0 +1,13 @@
+function forLoop () {
+    const arrayOfNumebers = [1,2,3,4,5,6,7,5,4,3334,4,5,4,3,2];
+
+    let newArray = [];
+
+    for(i = 0; i < arrayOfNumebers.length; i++){
+       newArray = [...newArray, arrayOfNumebers[i] += 1];
+    }
+
+    return newArray;
+}
+
+console.log(forLoop());
