547e1e872f52d3e5d420a21e23d3e5b52b294bf7
Himel Islam <himel7100@gmail.com>
2024-01-14 18:33:20 +0600

    data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as a newer version.
+
+// alert(3+3); // will not be exicuted as it's on nodeJS
+
+console.log("hee");
+console.log("gg");// code readability is must. 
+
+let name = "Hi";
+let age = 20;
+let isLogged = true;
+
+// Primitive data types
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => stand alone value;
+// undefined = > undefined
+// symbol => unique (es6)
+
+
+// non-primitive
+
+// object => {}
+// array => []
+// function => reusable block of code.
+// date => date();
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
