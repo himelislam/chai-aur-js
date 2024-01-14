03f3a954b583e3ec26fdeb832833743b71206a9d
Himel Islam <himel7100@gmail.com>
2024-01-14 23:39:20 +0600

    conversion datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33sss";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+const numberScore = Number(score);
+
+
+console.log(typeof numberScore);
+console.log(numberScore);
+
+
+// "33" => 33;
+// "33aa" => NaN
+// true => 1; false => 0;
+
