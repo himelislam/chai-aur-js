7d8daf93ba810896fb91fcb8ae9dd78d8ec4f476
Himel Islam <himel7100@gmail.com>
2024-01-14 23:47:56 +0600

    conversion operation of datatypes

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,16 +1,37 @@
 let score = "33sss";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 const numberScore = Number(score);
 
 
-console.log(typeof numberScore);
-console.log(numberScore);
+// console.log(typeof numberScore);
+// console.log(numberScore);
 
 
 // "33" => 33;
 // "33aa" => NaN
 // true => 1; false => 0;
+
+
+let loggedIn = 'asd';
+
+const valueInBoolean = Boolean(loggedIn);
+
+// console.log(typeof loggedIn);
+// console.log(valueInBoolean);
+
+// 1 => true; 0 => false;
+// "" => false; "asd" => true;
+// undefined => false
+// null => false;
+
+
+let booleanTest = null;
+
+const stringValue = String(booleanTest);
+
+console.log(typeof stringValue);
+console.log(stringValue);
 
