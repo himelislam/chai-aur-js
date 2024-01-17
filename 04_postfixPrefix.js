19e9e742e31edd5fb4b71b66c77989f3703487d0
Himel Islam <himel7100@gmail.com>
2024-01-17 12:11:22 +0600

    postfix and prefix

---
+++ 04_postfixPrefix.js
@@ -0,0 +1,11 @@
+let x = 3;
+const y = x++;
+
+// console.log(`x = ${x} , y = ${y}`);
+
+
+
+let a = 10;
+const b = ++a;
+
+console.log(`a = ${a} , b = ${b}`);
---
+++ 05_comparisoin.js
@@ -0,0 +1,13 @@
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+// comparisons [< > <= >=] convert null to a number 0
+// on equality it doesn't
+
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+// false for all
