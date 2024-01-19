f42f01f5ef4ed0421378495cda457b9f69a8cb7d
Himel Islam <himel7100@gmail.com>
2024-01-19 18:24:56 +0600

    string methoods

---
+++ 07_string.js
@@ -0,0 +1,16 @@
+const playerName = new String('Himel');
+
+console.log(playerName);
+
+console.log(playerName.__proto__);
+
+console.log(playerName.length);
+console.log(playerName.toUpperCase());
+
+console.log(playerName.anchor('https://google.com'));
+
+console.log(playerName.blink());
+
+console.log(playerName.charAt(3));
+
+console.log(playerName.indexOf('i'));
