c916809ed9c5ac9e3ef165b2080bf9a8d6f8e72e
Himel Islam <himel7100@gmail.com>
2024-01-19 19:10:35 +0600

    string methods

--- 07_string.js
+++ 07_string.js
@@ -1,16 +1,32 @@
-const playerName = new String('Himel');
+const playerName = new String('Himel-islam');
 
-console.log(playerName);
+// console.log(playerName);
 
-console.log(playerName.__proto__);
+// console.log(playerName.__proto__);
 
-console.log(playerName.length);
-console.log(playerName.toUpperCase());
+// console.log(playerName.length);
+// console.log(playerName.toUpperCase());
 
-console.log(playerName.anchor('https://google.com'));
+// console.log(playerName.anchor('https://google.com'));
+// console.log(playerName.blink());
 
-console.log(playerName.blink());
+// console.log(playerName.charAt(3));
+// console.log(playerName.indexOf('i'));
 
-console.log(playerName.charAt(3));
 
-console.log(playerName.indexOf('i'));
+// console.log(playerName.substring(0,4));
+console.log(playerName.slice(-8, 7));
+
+
+const newString = '    himel   ';
+console.log(newString);
+console.log(newString.trim());
+
+
+const url = "https://himel.com/himel%20islam";
+
+console.log(url.replace('%20', '-'));
+
+const spliteName = "himel-islam-khan-ch";
+
+console.log(spliteName.split('-'));
