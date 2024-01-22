b2fd5683704b4386e8dc4e041a8ece11cd1e8aa3
Himel Islam <himel7100@gmail.com>
2024-01-22 21:51:17 +0600

    math

--- 08_nums-and-math.js
+++ 08_nums-and-math.js
@@ -1,5 +1,35 @@
 const score = 400;
-console.log(score);
+// console.log(score);
 
-const balance = new Number(100);
-console.log(balance);
+const balance = new Number(100.2349434);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// console.log(balance.toPrecision(4));
+
+const secondBalance = 10000000;
+
+// console.log(secondBalance.toLocaleString('en-IN'));
+
+
+
+// ++++++++ Maths ++++++++
+
+// console.log(Math.abs(-23));
+// console.log(Math.round(3.2));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,23,635,24,234,543));
+// console.log(Math.max(1,23,635,24,234,543));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 20;
+const max = 50;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
