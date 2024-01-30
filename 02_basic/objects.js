624fca23717cf2bdd1a87fce4ebd3275abe74bcd
Himel Islam <himel7100@gmail.com>
2024-01-31 01:42:04 +0600

    object this

--- 02_basic/objects.js
+++ 02_basic/objects.js
@@ -8,6 +8,7 @@ const mySym = Symbol("key1")
 
 const jsUser = {
     name: "himel",
+    [mySym] : "myKey1",
     "full name" : "himle islam",
     age: 21,
     location: "Sylhet",
@@ -15,9 +16,26 @@ const jsUser = {
     isLoggedIn : false
 }
 
-console.log(jsUser.name);
-console.log(["full name"]);
-console.log(mySym);
+// console.log(jsUser.name);
+// console.log(["full name"]);
+// console.log(typeof jsUser[mySym]);
+
+jsUser.email = "himel@test2.com"
+// Object.freeze(jsUser);
+jsUser.email = "himel@test3.com"
+
+jsUser.greeting = function (){
+    console.log('kemcho??');
+}
+
+jsUser.greetingTwo = function (){
+    console.log(`kemcho? ${this.name} ??`);
+}
+
+console.log(jsUser.greetingTwo());
+console.log(jsUser.greeting());
+
+// console.log(jsUser);
 
 
 
