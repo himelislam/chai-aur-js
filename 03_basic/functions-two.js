ba12a92d3af96cc168127d8d361890ed5683ff5e
Himel Islam <himel7100@gmail.com>
2024-01-31 12:27:05 +0600

    functions two

---
+++ 03_basic/functions-two.js
@@ -0,0 +1,24 @@
+function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(122,33,1,22,322));
+
+const user = {
+    name: "hiii",
+    price:23456
+}
+
+function handleObject(user){
+    return `name = ${user.name} and price = ${user.price}`
+}
+
+// console.log(handleObject(user));
+
+const myNewArray = [12,23,34,45,56,75];
+
+function retureSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(retureSecondValue(myNewArray));
