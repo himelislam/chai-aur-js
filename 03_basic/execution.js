27b641609a320baa4e7755a18d72910faed7c17e
Himel Islam <himel7100@gmail.com>
2024-02-03 04:54:34 +0600

    execution context

---
+++ 03_basic/execution.js
@@ -0,0 +1,23 @@
+// how js wil run or execute the code? => in two phases 1. global execution context on this variable
+// browser => this value => window onject
+// first => global execution context
+// second memory phase => let num = undefined or addNume = defination of function
+// exection phaase => add the values to the variables and do all the tasks
+
+
+// +++++++++++ Call Stack ++++++++++++++++
+
+// Lifo concept 
+// Last In First Out
+
+
+
+
+
+// query{
+//     getPostsBySiteId(
+//       siteId: 65bd5af974cff800123bc030
+//     ){
+//       id title excerpt content siteId authorId authors creator isCommentEnable categories tags
+//     }
+//   }
