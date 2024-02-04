d4f3d155afb4ef7c429c18a069c2b137f71c587d
Himel Islam <himel7100@gmail.com>
2024-02-05 01:28:23 +0600

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while loop
+
+// concept are the same with 1: initial variable as integer 2: condition 3: incrementing the initial value
+
+let index = 0;
+
+while (index <= 20) {
+    // console.log(`value: ${index}`)
+    index = index + 2;
+}
+
+
+let heros = ["Flash", "Batman", "Superman", "Ironman"]
+
+let i = 0;
+
+while (i < heros.length) {
+    // console.log(`Names: ${heros[i]}`);
+    i++
+}
+
+let score = 1;
+
+do {
+    console.log("score: ", score);
+    score ++
+} while (score < 10);
+
