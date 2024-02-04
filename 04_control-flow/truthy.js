5154c0b782cc180d25f82d144f580f5fab33fb8a
Himel Islam <himel7100@gmail.com>
2024-02-05 00:27:11 +0600

    nullish operator

--- 04_control-flow/truthy.js
+++ 04_control-flow/truthy.js
@@ -23,3 +23,23 @@ const obj = {};
 if(Object.keys(obj).length === 0){
     console.log("Obj is empthy");
 }
+
+// false == 0 => true
+// "" == false => true
+// '' == 0 => true
+
+// Nullish Coalesching Operator ?? only for null and undefined
+
+let val1 = 5 ?? 10 // => 5
+let val2 = null ?? 10 // => 10
+let val3 = undefined ?? 10 // => 10
+let val4 = null ?? 10 ?? 20 // => 10 will be asigned first stright value
+
+
+// Terniary operator 
+
+// condition ? true : false 
+
+const price = 30;
+
+price >= 50 ? console.log("More than 50") : console.log("Less than 50");
