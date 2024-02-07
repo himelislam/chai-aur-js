132b98318ea4cb15464139605638316261b9aabc
Himel Islam <himel7100@gmail.com>
2024-02-08 03:26:46 +0600

    query selectors

--- 06_dom/one.js
+++ 06_dom/one.js
@@ -15,9 +15,9 @@
 // document.getElementById('title').getAttribute('heading')
 // document.getElementById('title').setAttribute('class' , 'test')
 
-const title = document.getElementById('title')
-title.style.backgroundColor = 'green'
-title.style.padding = '15px 15px'
+// const title = document.getElementById('title')
+// title.style.backgroundColor = 'green'
+// title.style.padding = '15px 15px'
 
 
 title.textContent //// it will access all the content even with hidden contents
@@ -26,3 +26,30 @@ title.innerText // => only visible content will be accessible
 title.innerHTML // it will access with all the html files within that node.
 
 document.querySelector('h1') // only the first one will be accessed
+document.querySelectorAll('h2')// returns all the nodes
+
+document.querySelector('input[type="password"]')// for attributes
+document.querySelector('p:first-child')// based on css selectors
+
+
+// querySelectorAll provides Nodelists which only supports forEach for loop method
+// for styling 
+
+// only querySelector considers with return value of that html
+
+const allListItems = document.querySelectorAll('li') // => Nodelists => forEach
+allListItems[1].style.backgroundColor = 'green' // to access the Nodelists
+
+const allListItem = document.getElementsByClassName('list-item') // HTMLCollection
+
+// HTMLCollection to Array
+
+const ArrOfHtmlCollection = Array.from(allListItem)
+
+console.log(ArrOfHtmlCollection);
+
+// NOTES
+
+// querySelector = returns the html tag
+// querySelectorAll =  returns nodelists => variable[1].styles => forEach
+// getElementsByClassNames = returns HTMLCollections => variable[1] => no loop function
