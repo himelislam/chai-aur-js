4a7d3e0f252a537e6651bf7791d67d797943fa12
Himel Islam <himel7100@gmail.com>
2024-02-12 22:35:13 +0600

    project two final push

--- 07_projects/bmi/app.js
+++ 07_projects/bmi/app.js
@@ -1,12 +1,17 @@
 const form = document.querySelector('form')
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault()
     const height = document.querySelector('#height').value
     const weight = document.querySelector('#weight').value
     const results = document.querySelector('#results')
-
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
-    results.innerHTML = `<span>${bmi}</span>`
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `<p>Please enter a valid height value</p>`
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `<p>Please enter a valid weight value</p>`
+    } else {
+        results.innerHTML = `<span>${bmi}</span>`
+    }
 })
