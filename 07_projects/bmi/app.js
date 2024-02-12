2cf589f888a97b45900343fbe28a8b17e9e205b0
Himel Islam <himel7100@gmail.com>
2024-02-12 22:23:45 +0600

    bmi calculator

---
+++ 07_projects/bmi/app.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const height = document.querySelector('#height').value
+    const weight = document.querySelector('#weight').value
+    const results = document.querySelector('#results')
+
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    results.innerHTML = `<span>${bmi}</span>`
+})
