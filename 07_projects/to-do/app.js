a628934462494110fe866ff806a6266c2a6f4630
Himel Islam <himel7100@gmail.com>
2024-02-25 03:47:05 +0600

    toggeling task with overline: added

--- 07_projects/to-do/app.js
+++ 07_projects/to-do/app.js
@@ -1,26 +1,37 @@
 const task = document.getElementById('task');
 const tasks = document.querySelector('.tasks');
 
-document.addEventListener('keydown', function(e){
-    if(e.key === 'Enter'){
+document.addEventListener('keydown', function (e) {
+ß
+    if (e.key === 'Enter') {
+
+        // appending new task upon clicking the enter button
         const newTask = task.value;
         const newCreatedTask = document.createElement('li')
         newCreatedTask.innerHTML = newTask;
         newCreatedTask.classList = 'added-task';
         tasks.appendChild(newCreatedTask)
         task.value = '';
-    
-        const addedTasks = document.querySelectorAll('ul li');
 
-        addedTasks.forEach((task) => {
-            task.addEventListener('click', function(e){
+
+        // toggeling task with class names
+        const addedtask = document.querySelector('ul')
+
+        addedtask.addEventListener('click', function (e) {
+            console.log(e.target);
+            if (e.target.classList.contains('clicked')) {
+                console.log('yes');
+                e.target.classList = 'added-task'
+            } else {
+                console.log('no');
                 e.target.classList = 'added-task clicked'
-            })
+            }
+        })
 
-            task.addEventListener('contextmenu', function(e){
-                e.preventDefault()
-                console.log(e.target.remove());
-            })
+        // removing tasks with right button click or double tap.
+        addedtask.addEventListener('contextmenu', function (e) {
+            e.preventDefault()
+            console.log(e.target.remove());
         })
     }
 });
@@ -28,4 +39,23 @@ document.addEventListener('keydown', function(e){
 
 
 
+
+
+// previous approach
+// addedTasks.forEach((task) => {
+        //     task.addEventListener('click', function(e){
+        //         // if(e.target.classList.contains('clicked')){
+        //         //     console.log('yes');
+        //         //     e.target.classList = 'added-task'
+        //         // }else{
+        //         //     console.log('no');
+        //         //     e.target.classList = 'added-task clicked'
+        //         // }
+        //     })
+
+        //     task.addEventListener('contextmenu', function(e){
+        //         e.preventDefault()
+        //         console.log(e.target.remove());
+        //     })
+        // })
 
