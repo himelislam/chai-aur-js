9f60c3182a68870c0ca24e218df6c69267a26c2d
Himel Islam <himel7100@gmail.com>
2024-02-25 04:14:38 +0600

    toogle tried

--- 07_projects/to-do/app.js
+++ 07_projects/to-do/app.js
@@ -1,48 +1,35 @@
 const task = document.getElementById('task');
 const tasks = document.querySelector('.tasks');
 
-document.addEventListener('keydown', function (e) {
-ß
-    if (e.key === 'Enter') {
-
-        // appending new task upon clicking the enter button
+document.addEventListener('keydown', function(e){
+    
+    if(e.key === 'Enter'){
         const newTask = task.value;
         const newCreatedTask = document.createElement('li')
         newCreatedTask.innerHTML = newTask;
         newCreatedTask.classList = 'added-task';
         tasks.appendChild(newCreatedTask)
         task.value = '';
-
-
-        // toggeling task with class names
+    
         const addedtask = document.querySelector('ul')
 
-        addedtask.addEventListener('click', function (e) {
+        addedtask.addEventListener('click', function(e){
             console.log(e.target);
-            if (e.target.classList.contains('clicked')) {
+            if(e.target.classList.contains('clicked')){
                 console.log('yes');
                 e.target.classList = 'added-task'
-            } else {
+            }else{
                 console.log('no');
                 e.target.classList = 'added-task clicked'
             }
         })
 
-        // removing tasks with right button click or double tap.
-        addedtask.addEventListener('contextmenu', function (e) {
+        addedtask.addEventListener('contextmenu', function(e){
             e.preventDefault()
             console.log(e.target.remove());
         })
-    }
-});
 
-
-
-
-
-
-// previous approach
-// addedTasks.forEach((task) => {
+        // addedTasks.forEach((task) => {
         //     task.addEventListener('click', function(e){
         //         // if(e.target.classList.contains('clicked')){
         //         //     console.log('yes');
@@ -58,4 +45,10 @@ document.addEventListener('keydown', function (e) {
         //         console.log(e.target.remove());
         //     })
         // })
+    }
+});
+
+
+
+
 
