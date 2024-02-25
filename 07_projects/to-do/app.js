04eb074c1fe7f319aea3c2e55ea0e1d202217d86
Himel Islam <himel7100@gmail.com>
2024-02-25 15:38:43 +0600

    tried with anoher approach

--- 07_projects/to-do/app.js
+++ 07_projects/to-do/app.js
@@ -4,22 +4,21 @@ const tasks = document.querySelector('.tasks');
 document.addEventListener('keydown', function(e){
     
     if(e.key === 'Enter'){
-        const newTask = task.value;
-        const newCreatedTask = document.createElement('li')
-        newCreatedTask.innerHTML = newTask;
-        newCreatedTask.classList = 'added-task';
-        tasks.appendChild(newCreatedTask)
-        task.value = '';
-    
-        const addedtask = document.querySelector('ul')
-
+        if(task.value != ''){
+            const newTask = task.value;
+            const newCreatedTask = document.createElement('li')
+            newCreatedTask.innerHTML = newTask;
+            newCreatedTask.classList = 'added-task';
+            tasks.appendChild(newCreatedTask)
+            task.value = '';
+        }
+        
+        const addedtask = document.querySelector('ul');
         addedtask.addEventListener('click', function(e){
-            console.log(e.target);
+            // console.log(e.target.style.textDecoration = 'line-through');
             if(e.target.classList.contains('clicked')){
-                console.log('yes');
                 e.target.classList = 'added-task'
             }else{
-                console.log('no');
                 e.target.classList = 'added-task clicked'
             }
         })
@@ -28,6 +27,10 @@ document.addEventListener('keydown', function(e){
             e.preventDefault()
             console.log(e.target.remove());
         })
+
+        // another approach
+
+        // const addedTasks = document.querySelectorAll('ul li');
 
         // addedTasks.forEach((task) => {
         //     task.addEventListener('click', function(e){
