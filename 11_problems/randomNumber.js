e58f340bc65caf28924e72cebbfc1834303010d9
Himel Islam <himel7100@gmail.com>
2024-03-24 03:36:42 +0600

    problem sloving

---
+++ 11_problems/randomNumber.js
@@ -0,0 +1,12 @@
+// random numbers between one to six
+
+const randomNumber = Math.ceil(Math.random() * 6 )
+
+// console.log(randomNumber);
+
+let arr = [];
+for (let index = 0; index < 30; index++) {
+    const random = Math.ceil(Math.random() * 6 ) 
+    arr.push(random)
+}
+console.log(arr);
